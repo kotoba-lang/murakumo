5943fb2e39024ce2ce7a4db1b86daa220a17afde
Jun Kawasaki <jun784@gmail.com>
2026-07-27 12:59:28 +0900

    feat: deploy resident Kototama Component daemon

---
+++ test/murakumo/component_runtime_deploy_test.clj
@@ -0,0 +1,43 @@
+(ns murakumo.component-runtime-deploy-test
+  (:require [clojure.test :refer [deftest is testing]]
+            [murakumo.component-runtime-deploy :as deploy]))
+
+(def template
+  "<plist>{{USER}} {{BIN}} {{COMPONENT}} {{COMPONENT_CID}} {{COMPONENT_SHA256}} {{EXPECTED_RESULT}} {{FUEL}} {{MEMORY_PAGES}} {{NODE}} {{SEED}} {{RECEIPTS}} {{LOG}}</plist>")
+
+(def input
+  {:node {:name "asher" :host "asher"}
+   :user "asher"
+   :home "/Users/asher"
+   :binary "/tmp/tender-component-host"
+   :bundle {:component "/tmp/app.wasm"
+            :wit "/tmp/app.wasm.wit"
+            :admission "/tmp/app.wasm.admission.edn"
+            :provenance "/tmp/app.wasm.provenance.edn"}
+   :component-cid "bafkreieuedg6qftafxc66n55kphyi7yehimgytmgittcon5wjol5kup7a4"
+   :component-sha256 "9420cde816602dc5ef37bd53cf847f043a186c4d8644e62737b64b97d551ff07"
+   :expected-result 6419002
+   :budgets {:fuel 512 :memory-pages 16 :deadline-ms 10000}
+   :template template})
+
+(deftest rollout-is-loopback-resident-and-secret-free
+  (let [plan (deploy/deployment-plan input)
+        rendered (deploy/render-plist template input)]
+    (is (= "http://127.0.0.1:18901" (:endpoint plan)))
+    (is (= 5 (count (:copies plan))))
+    (is (every? #(not (re-find #"[0-9a-f]{64}" %))
+                (:prepare-commands plan))
+        "the receipt seed is generated on-node, not embedded in the plan")
+    (is (re-find #"openssl rand -hex 32" (first (:prepare-commands plan))))
+    (is (re-find #"com\.murakumo\.kototama-component" (second (:activate-commands plan))))
+    (is (re-find #"6419002" rendered))
+    (is (re-find #"bafkreieuedg6" rendered))))
+
+(deftest rollout-fails-closed
+  (testing "ambient or incomplete identities cannot be smuggled into the plan"
+    (doseq [bad [(assoc input :component-sha256 "bad")
+                 (assoc input :component-cid "not-a-cid")
+                 (assoc-in input [:budgets :fuel] 0)
+                 (assoc input :template "<plist/>")]]
+      (is (thrown? clojure.lang.ExceptionInfo
+                   (deploy/deployment-plan bad))))))
